9bc04b0cb563039758f66e6190c8c620438b44ad
Shakeel Khuhro <shakeelahmedkhuhro5454@gmail.com>
2023-04-28 10:19:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function setTimer() {
+    const element1 = `
+<div>
+<h1>JavaScript USing Actual DOM</h1>
+<input type="text"/>
+<p>${new Date().toLocaleTimeString()}</p>
+</div>
+`;
+
+    document.getElementById('container1').innerHTML = element1;
+
+    const element2 = React.createElement(
+        'div',
+        null,
+        React.createElement('h1', null, 'React using Virtual DOM'),
+        React.createElement('div', null,
+            React.createElement('input', { type: 'text' })
+        ),
+        React.createElement('p', null, new Date().toLocaleTimeString())
+    );
+
+    ReactDOM.render(element2, document.getElementById('container2'));
+
+}
+
+setInterval(setTimer, 1000);
